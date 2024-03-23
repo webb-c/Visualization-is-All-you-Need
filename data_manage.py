9845b00fd041905f05f4f1edbd13080588baec05
webb-c <jwst0208@gmail.com>
2024-03-23 10:36:53 +0900

    add: basic plot function

--- data_manage.py
+++ data_manage.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+from IPython.display import display
 from tensorflow.python.summary.summary_iterator import summary_iterator
 
 def load_ndarray(file_path:str):
@@ -15,26 +16,27 @@ def load_csv(file_path:str):
     return data
 
 
-
-def load_log(log_dir, sort_by=None):
-    """주어진 path에 있는 log를 읽어서, 각각의 tag를 column으로 하고 row에는 step별 value가 기록된 DataFrame을 만들어 반환합니다.
+def load_log(log_dir, tag_filter=None, sort_by=None):
+    """주어진 path에 있는 log를 읽어서, 특정 tag에 대한 value만 추출하여 DataFrame을 만들어 반환합니다.
     #! This code reference from https://gist.github.com/laszukdawid/62656cf7b34cac35b325ba21d46ecfcd 
     Args:
-        log_dir: (str) path to root dir with tensorboard data.
-        sort_by: (optional str) column name to sort by.
+        log_dir: (str) tensorboard 데이터가 있는 루트 디렉토리 경로.
+        tag_filter: (str or list of str) 반환할 태그 또는 태그들의 리스트.
+        sort_by: (optional str) 정렬할 기준 열 이름.
     
     Returns:
-        pandas.DataFrame with columns as tags and rows as values.
+        pandas.DataFrame: tag_filter에 해당하는 태그에 대한 값만 포함된 DataFrame.
     """
-    def convert_tfevent(filepath):
+    def convert_tfevent(filepath, tag_filter):
         data = {}
         for e in summary_iterator(filepath):
             if len(e.summary.value) > 0:
                 for value in e.summary.value:
                     tag = value.tag
-                    if tag not in data:
-                        data[tag] = []
-                    data[tag].append(value.simple_value)
+                    if tag_filter is None or tag in tag_filter:
+                        if tag not in data:
+                            data[tag] = []
+                        data[tag].append(value.simple_value)
         return pd.DataFrame(data)
 
     out = []
@@ -43,14 +45,13 @@ def load_log(log_dir, sort_by=None):
             if "events.out.tfevents" not in filename:
                 continue
             file_full_path = os.path.join(root, filename)
-            out.append(convert_tfevent(file_full_path))
+            out.append(convert_tfevent(file_full_path, tag_filter))
 
     all_df = pd.concat(out)
     if sort_by is not None:
         all_df = all_df.sort_values(sort_by)
         
     return all_df.reset_index(drop=True)
-
 
 
 def extract_column_from_df(df, tag_list):
@@ -94,3 +95,114 @@ def df_info(df):
     display(df.tail())
 
 
+def revise_df(df, operation, column, value, prior_value=False):
+    """DataFrame의 특정 column의 전체 값에 대해 어떤 연산을 취한다.
+    """
+    revised_df = df.copy()
+    if operation == 'add':
+        revised_df[column] += value
+    elif operation == 'subtract':
+        if prior_value:
+            revised_df[column] = value - df[column]
+        else:
+            revised_df[column] -= value
+    elif operation == 'multiply':
+        revised_df[column] *= value
+    elif operation == 'divide':
+        if prior_value:
+            revised_df[column] = value / df[column]
+        else:
+            revised_df[column] /= value
+    elif operation == 'int_divide':
+        if prior_value:
+            revised_df[column] = value // df[column]
+        else:
+            revised_df[column] //= value
+
+    return revised_df
+
+
+def make_save_name(file_path):
+    split_list = file_path.split("/")
+    type = split_list[1]
+    tag_dict = {
+        "videopath": 'data',
+        "rewardmethod": 'reward',
+        "threshold": 'thresh',
+        "epsdec": 'eps'
+    }
+    if type == "logs":
+        tag_list = split_list[2].split("_")
+        save_name = ""
+        import_flag = 0
+        idx = 0
+        for tag in tag_list:
+            name = tag
+            value = tag_dict.get(tag)
+            if value is not None:
+                name = value
+            if tag == 'importantmethod':
+                import_flag += 1
+                name = ""
+            elif import_flag == 1:
+                import_flag += 1
+                name = ""
+            elif idx == 0:
+                idx += 1
+            elif idx == 1:
+                idx += 1
+                save_name += name
+            else:
+                save_name += ("_" + name)
+                
+    elif type == "models":
+        tag_list = split_list[2][:-4].split("_")
+        save_name = ""
+        import_flag = 0
+        idx = 0
+        for tag in tag_list:
+            name = tag
+            value = tag_dict.get(tag)
+            if value is not None:
+                name = value
+            if tag == 'importantmethod':
+                import_flag += 1
+                name = ""
+            elif import_flag == 1:
+                import_flag += 1
+                name = ""
+            elif idx == 0:
+                idx += 1
+            elif idx == 1:
+                idx += 1
+                save_name += name
+            else:
+                save_name += ("_" + name)
+        
+    elif type == "csvs":
+        save_name = split_list[2][:-4]
+        
+        
+    return save_name
+
+
+def combine_columns_into_df(column_list, df_name_list, df_list):
+    """여러 DataFrame에서 특정 column의 값을 모아 하나의 DataFrame으로 만듭니다.
+    
+    Args:
+        columns (list of str): 데이터를 모을 column들의 이름.
+        column_names (list of str): 각 column에 대한 이름들.
+        df_names (list of str): 각 DataFrame을 구분하기 위한 이름들.
+        df_list (list of pd.DataFrame): 데이터를 가져올 여러 DataFrame들.
+
+    Returns:
+        pd.DataFrame: 모든 값이 모인 새로운 DataFrame.
+    """
+    combined_data = {}
+
+    for col in column_list:
+        for name, df in zip(df_name_list, df_list):
+            new_name = f"{name}:{col}" 
+            combined_data[new_name] = df[col]
+
+    return pd.DataFrame(combined_data)
--- visualization.py
+++ visualization.py
@@ -1,5 +1,113 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def save_plot(plot, save_path):
+    """Save the given plot to a file.
+    
+    Args:
+        plot: The plot object to save.
+        filename (str): The name of the file to save the plot to.
+        format (str): The format of the file to save the plot to (default: 'png').
+    """
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    plt.savefig(save_path+'.png')
+
+
+def make_barcode(skip_list, num_frame, color='red', fps=30, save=False, save_path=None) :
+    """Make barcode figrue. (white is skip_frame)
+    Args
+        skip_list (pd.DataFrame)
+    """
+    squares = [1] * num_frame
+    idx = 0
+    for s in skip_list.squeeze().values.tolist():
+        for i in range(int(s)):
+            if idx + i >= num_frame:
+                break
+            squares[idx + i] = 0
+        idx += 30
+
+    plt.figure(figsize=(num_frame / 20, 7))
+    for i in range(num_frame):
+        if squares[i] == 1:
+            plt.bar(i, 1, color=color)
+
+    plt.axis([0, num_frame, 0, 1])
+
+    plt.xticks([])
+    plt.yticks([])
+    if save:
+        save_plot(plt, save_path)
+
+    plt.show()
+
+
+def plot_dataframe(df, xlabel='step', ylabel='value', title=None, color=None, figsize=(8, 6), save_path=None, save=False):
+    """Plot each column of the given DataFrame
+    
+    Args:
+        df (pd.DataFrame)
+        xlabel(str, optional): plot xlabel
+        ylabel (str, optional): plot ylabel
+        title (str, optional): plot title
+        save (bool, optional): save option
+    """
+    plt.figure(figsize=figsize)
+    
+    for i, col in enumerate(df.columns):
+        if color:
+            plt.plot(df.index, df[col], label=col, color=color[i % len(color)])
+        else:
+            plt.plot(df.index, df[col], label=col)
+    
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    if title is not None:
+        plt.title(title)
+    
+    plt.tight_layout()
+    
+    if save:
+        save_plot(plt, save_path)
+
+    plt.show()
+
+
+def plot_dataframe_each_plot(df, xlabel='step', ylabel='value', color=None, figsize=(10, 4), title=None, save_path=None, save=False):
+    """Plot each column of the given DataFrame individual graph.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to plot.
+        xlabel (str, optional): The x-axis label.
+        ylabel (str, optional): The y-axis label.
+        title (str, optional): The plot title.
+        save_path (str, optional): The path to save the plot.
+        save (bool, optional): Whether to save the plot.
+    """
+    num_cols = len(df.columns)
+    fig, axes = plt.subplots(nrows=num_cols, sharex=True, figsize=(figsize[0], figsize[1]*num_cols))
+
+    for i, col in enumerate(df.columns):
+        if color:
+            axes[i].plot(df.index, df[col], label=col, color=color)
+        else:
+            axes[i].plot(df.index, df[col], label=col)
+        axes[i].set_xlabel(xlabel)
+        axes[i].set_ylabel(ylabel)
+        if title is not None:
+            axes[i].set_title(f"{title} ({col})")
+    
+    plt.tight_layout()
+    
+    if save:
+        save_path += "_Each"
+        save_plot(plt, save_path)
+
+    plt.show()
+
 
 def plot_heatmap(data, name=None, show_minmax=False):
     plt.figure(figsize=(12, 10)) 
